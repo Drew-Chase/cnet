a80dc6afc3cd19a0d50c7a99f8c2c3289498fabc
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-09 15:17:21 -0400

    Add http_method enum to new includes file
    A new header file "http_method.h" has been created in includes directory. In this file, an enumeration (enum) has been added under cnet namespace. This enum defines all the different types of HTTP methods including GET, POST, PUT, DELETE, and others.

---
+++ includes/http_method.h
@@ -0,0 +1,24 @@
+﻿//
+// Created by drew.chase on 4/9/2024.
+//
+
+#ifndef HTTP_METHOD_H
+#define HTTP_METHOD_H
+
+namespace cnet
+{
+    enum http_method
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE,
+        HEAD,
+        OPTIONS,
+        PATCH,
+        TRACE,
+        CONNECT
+    };
+}
+
+#endif //HTTP_METHOD_H
