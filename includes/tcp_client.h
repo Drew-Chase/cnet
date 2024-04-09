3eb4b58064f0c09ee23a090e2984a702623a74a7
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-09 16:30:31 -0400

    Add tcp_client.h with TCP client class
    This commit introduces the tcp_client.h header file, which declares the `tcp_client` class within the `cnet` namespace. The class includes methods to connect, send messages, receive responses, and close a TCP connection.

---
+++ includes/tcp_client.h
@@ -0,0 +1,45 @@
+﻿//
+// Created by drew.chase on 4/9/2024.
+//
+
+#ifndef TCP_CLIENT_H
+#define TCP_CLIENT_H
+#include <string>
+
+namespace cnet
+{
+    class tcp_client
+    {
+    private:
+        std::string host;
+        int port;
+
+    public:
+        /**
+         * @brief Establishes a TCP connection to a specified host and port.
+         *
+         * This method creates a TCP client object, sets the host and port variables of the client,
+         * and initializes the necessary data structures for establishing a connection.
+         *
+         * On windows this will use the Winsock library to establish a connection.<br>
+         * <b>See</b> <a href="https://learn.microsoft.com/en-us/windows/win32/winsock/complete-client-code">Microsoft Docs</a> for more information.
+         *
+         * On Unix systems this will use the socket library to establish a connection.<br>
+         * <b>See</b> "https://www.linuxhowtos.org/C_C++/socket.htm" for more information.
+         *
+         * @param host The hostname or IP address of the server to connect to.
+         * @param port The port number to connect to on the server.
+         *
+         * @return A TCP client object that represents the established connection.
+         */
+        static tcp_client connect(const std::string &host, int port);
+
+        void send(std::string message);
+
+        std::string receive();
+
+        void close();
+    };
+} // cnet
+
+#endif //TCP_CLIENT_H
