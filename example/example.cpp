bcb185725954520f83a5fba133cd366e9f9b4f93
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-09 15:17:53 -0400

    Implement HTTP request in main function
    Added an HTTP request to the main function in "example.cpp". This includes the creation and sending of an HTTP message, as well as conditional handling of the message's response status.

--- example/example.cpp
+++ example/example.cpp
@@ -2,13 +2,23 @@
 // Created by drew.chase on 4/9/2024.
 //
 
+#include <iostream>
+
 #include "http_client.h"
 
 
 int main()
 {
     cnet::http_client client;
-
+    cnet::http_message message("https://postman-echo.com/get");
+    client.send(message);
+    if (message.is_sucess())
+    {
+        std::cout << message.body << std::endl;
+    } else
+    {
+        fprintf(stderr, "%s responeded with status code %d\nBody:%s\n", message.url.c_str(), message.status_code, message.body.c_str());
+    }
 
 
     return 0;
