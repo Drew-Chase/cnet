117e26e224b11107e351cd2339f9026135f083e0
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-10 16:25:21 -0400

    Add HTTP client support to main function
    The main function has now incorporated a http client instance from the cnet library to make web requests. In the case of a failure, it would now also catch and print the runtime error message.

--- example/example.cpp
+++ example/example.cpp
@@ -2,10 +2,23 @@
 // Created by drew.chase on 4/9/2024.
 //
 
+#include <stdexcept>
+
+#include "http_client.h"
 #include "uri-example.cpp"
 
 int main()
 {
+    try
+    {
+        cnet::http_client client;
+        cnet::http_message message("https://postman-echo.com/get");
+        client.make_request(message);
+    } catch (std::runtime_error &e)
+    {
+        printf("%s\n", e.what());
+    }
+
     test_uri();
 
 
