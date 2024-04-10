297f7c049f55750f65b00178fb2026c685e9a982
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-10 15:01:29 -0400

    Replace http_client with uri test in main
    The main function is updated to remove the previous implementation that used a http_client to make a request to a specific URL. Instead, the main function now calls a new function, test_uri(), which demonstrates an example URI usage.

--- example/example.cpp
+++ example/example.cpp
@@ -2,24 +2,11 @@
 // Created by drew.chase on 4/9/2024.
 //
 
-#include <iostream>
-
-#include "http_client.h"
-
+#include "uri-example.cpp"
 
 int main()
 {
-    cnet::http_client client;
-    cnet::http_message message("https://postman-echo.com/get");
-    client.make_request(message);
-    if (message.is_sucess())
-    {
-        std::cout << message.body << std::endl;
-    } else
-    {
-        fprintf(stderr, "%s responeded with status code %d\nBody:%s\n", message.url.c_str(), message.status_code, message.body.c_str());
-    }
-
+    test_uri();
 
     return 0;
 }
