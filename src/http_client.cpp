4ad9cf018d6dda1a4570ec702a469c226894ed5c
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-10 15:43:20 -0400

    Add preflight check to http_client
    Implemented a 'preflight_check' function in 'http_client.cpp' to verify that the host is not empty and the port is not zero before making a request. The function throws a runtime error if any of these checks fail. This will prevent unnecessary requests and improve error handling.

--- src/http_client.cpp
+++ src/http_client.cpp
@@ -11,5 +11,17 @@ namespace cnet
 {
     void http_client::make_request(http_message &message)
     {
+        if(!preflight_check(message))
+        {
+            throw std::runtime_error("Preflight check failed");
+        }
+    }
+
+    bool http_client::preflight_check(http_message &message)
+    {
+        if(message.url.get_host().empty()) throw std::runtime_error("Host is empty");
+        if(message.url.get_port() == 0) throw std::runtime_error("Port is 0");
+
+
     }
 } // cnet
