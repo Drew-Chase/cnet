05d815cff1faa6413cafb348a0c99e373b5cbbe2
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-04-15 07:40:26 -0400

    Replace http_client with tcp_client in main function
    The previous HTTP client usage has been commented out and replaced with a more direct TCP client. The TCP client connects to google.com and sends an HTTP GET request, then waits for a response before closing. The replacement should improve web request efficiency.

--- example/example.cpp
+++ example/example.cpp
@@ -11,15 +11,21 @@ using namespace cnet;
 
 int main()
 {
-    try
-    {
-        http_client client;
-        http_message message("https://postman-echo.com/get");
-        client.make_request(message);
-    } catch (std::runtime_error &e)
-    {
-        printf("%s\n", e.what());
-    }
+    tcp_client tcp = tcp_client::connect("google.com", 443);
+    tcp.send("GET /get HTTP/1.1\r\nHost: google.com\r\nConnection: keep-alive\r\nUser-Agent: CNET\r\n\r\n");
+    const std::string response = tcp.receive(512); // Setting the buffer size to 4KB, this should be overkill for an OPTIONS request, but it's a good size for a buffer.
+    tcp.close();
+    printf("%s\n", response.c_str());
+
+    // try
+    // {
+    //     http_client client;
+    //     http_message message("https://google.com");
+    //     client.make_request(message);
+    // } catch (std::runtime_error &e)
+    // {
+    //     printf("%s\n", e.what());
+    // }
 
     // test_uri();
 
