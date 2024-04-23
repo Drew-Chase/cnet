2384343f4fa7d001e3dd468f4d535518aa82f20f
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-23 16:06:43 -0400

    Add SSL client header file
    A new header file, ssl_client.h, has been added to the includes directory. This file contains declaration of the SSL client class which extends from the TCP client class. The class has basic client functionality such as connect, send, receive and close.

---
+++ includes/ssl_client.h
@@ -0,0 +1,25 @@
+﻿#ifndef SSL_H
+#define SSL_H
+#include <openssl/types.h>
+
+#include "tcp_client.h"
+
+
+class ssl_client : public cnet::tcp_client
+{
+private:
+    SSL* ssl = nullptr;
+    SSL_CTX* ssl_context = nullptr;
+public:
+    static ssl_client connect(const std::string &host, int port);
+    void send(const std::string &message);
+    std::string receive(int buffer_size);
+    void close();
+
+
+
+
+};
+
+
+#endif
