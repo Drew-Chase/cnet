5201db17bc81b5eea76fd29bca2691a61d05695f
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-04-23 08:46:30 -0400

    Add new command-line options to cnet-cli
    Introduced additional command line options to extend functionality. These include options to set the output file, input url list, request method, body, headers, and more. The enhancements aim to provide a more comprehensive and versatile user experience.

--- cnet-cli/main.cpp
+++ cnet-cli/main.cpp
@@ -15,6 +15,18 @@ int main(const int argc, char *argv[])
     options_manager.add_option("h", "help", "Prints this message", false, false);
     options_manager.add_option("u", "url", "Sets the url of the interaction", false, true);
     options_manager.add_option("vd", "validate", "Validates the inputted fields", false, false);
+    options_manager.add_option("o", "output", "Sets the output file", false, true);
+    options_manager.add_option("i", "input", "Sets the input url list", false, true);
+    options_manager.add_option("c", "continue", "To allow the download to be continued if it was stopped", false, false);
+    options_manager.add_option("s", "silent", "Silences the output", false, false);
+    options_manager.add_option("v", "verbose", "Enables verbose output", false, false);
+    options_manager.add_option("m", "method", "Sets the method of the request, (GET, POST, HEAD, etc)", false, true);
+    options_manager.add_option("b", "body", "Sets the body of the request", false, true);
+    options_manager.add_option("hd", "header", R"(Sets the header of the request in a json format. Ex {Accept: "application/json", Content-Type: "application/json"})", false, true);
+    options_manager.add_option("t", "timeout", "Sets the timeout of the request", false, true);
+    options_manager.add_option("p", "parts", "Sets the number of parts to download the file in, this can increase the speed of the download", false, true);
+    options_manager.add_option("f", "force", "Forces the download to start even if the file already exists", false, false);
+
 
     options_manager.parse(argc, argv);
 
