2a4123aca590bdfa47a3c9193827090cea634540
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-24 11:04:11 -0400

    Refactor HTTP client request and response handling
    The HTTP client code has been refactored to improve how HTTP requests and responses are handled. Changes include storing the message within the http_client object, updating preflight check to not require a message parameter, and adding a method for parsing headers from the response. Exceptions are now properly caught and reported, improving error handling in case of failure.

--- src/http_client.cpp
+++ src/http_client.cpp
@@ -4,6 +4,7 @@
 
 #include "http_client.h"
 
+#include <iostream>
 #include <stdexcept>
 
 
@@ -11,14 +12,18 @@ namespace cnet
 {
     void http_client::make_request(http_message &message)
     {
+        this->message = message;
         tcp = tcp_client::connect(message.url.get_host(), message.url.get_port());
-        if (!preflight_check(message))
+        if (!preflight_check())
         {
             throw std::runtime_error("Preflight check failed");
         }
+        tcp.send(build_http_query(this->message));
+        const std::string response = tcp.receive(this->message.content_length);
+        parse_headers(response);
     }
 
-    bool http_client::preflight_check(http_message &message)
+    bool http_client::preflight_check()
     {
         if (message.url.get_host().empty()) throw std::runtime_error("Host is empty");
         try
@@ -26,12 +31,54 @@ namespace cnet
             tcp.send("HEAD " + message.url.get_path() + " HTTP/1.1\r\n"
                      "Host:" + message.url.get_host() + "\r\n\r\n");
             const std::string response = tcp.receive(4096); // Setting the buffer size to 4KB, this should be overkill for an OPTIONS request, but it's a good size for a buffer.
-            printf("%s\n", response.c_str());
+            parse_headers(response);
             return true;
-        } catch (...)
+        } catch (std::exception &e)
         {
+            std::cout << e.what() << std::endl;
             return false;
         }
+    }
+
+    void http_client::parse_headers(std::string body)
+    {
+        const std::string delimiter = "\r\n";
+        size_t pos = 0;
+        while ((pos = body.find(delimiter)) != std::string::npos)
+        {
+            try
+            {
+                std::string token = body.substr(0, pos);
+                body.erase(0, pos + 2);
+                if (token.empty()) continue;
+                if (token.find("HTTP") != std::string::npos)
+                {
+                    message.status_code = std::stoi(token.substr(token.find(' ') + 1, 3));
+                } else
+                {
+                    const size_t colon_pos = token.find(':');
+                    const std::string key = token.substr(0, colon_pos);
+                    const std::string value = token.substr(colon_pos + 1);
+                    message.headers[key] = value;
+
+                    if (key == "Content-Length")
+                    {
+                        message.content_length = std::stoi(value);
+                    }
+                    if (key == "Content-Type")
+                    {
+                        message.content_type = value;
+                    }
+
+                    printf("%s: %s\n", key.c_str(), value.c_str());
+                }
+            } catch (std::exception &e)
+            {
+                std::cout << e.what() << std::endl;
+            }
+        }
+        std::cout << body.c_str() << std::endl;
+        message.body = body;
     }
 
     std::string http_client::build_http_query(http_message &message)
