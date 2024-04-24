2a82dc4d511399cc5dfb578d7389373c75d5cc8d
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-24 13:21:18 -0400

    Add SSL support for HTTP client
    The HTTP client now supports secure HTTPS requests on port 443 by creating an SSL handshake. If an SSL connection is detected, the messages are sent and received using SSL methods. In addition, this change simplifies header parsing and removes unnecessary output to the console.

--- src/http_client.cpp
+++ src/http_client.cpp
@@ -14,13 +14,26 @@ namespace cnet
     {
         this->message = message;
         tcp = tcp_client::connect(message.url.get_host(), message.url.get_port());
+        if (message.url.get_port() == 443) tcp.create_ssl_handshake();
         if (!preflight_check())
         {
             throw std::runtime_error("Preflight check failed");
         }
-        tcp.send(build_http_query(this->message));
-        const std::string response = tcp.receive(this->message.content_length);
+        std::string response;
+        if (message.url.get_port() == 443)
+        {
+            tcp.write_ssl(build_http_query(this->message));
+            response = tcp.read_ssl(4096);
+        } else
+        {
+            tcp.send(build_http_query(this->message));
+            response = tcp.receive(4096);
+        }
+        this->message.body = response;
         parse_headers(response);
+
+
+        tcp.close();
     }
 
     bool http_client::preflight_check()
@@ -28,14 +41,22 @@ namespace cnet
         if (message.url.get_host().empty()) throw std::runtime_error("Host is empty");
         try
         {
-            tcp.send("HEAD " + message.url.get_path() + " HTTP/1.1\r\n"
-                     "Host:" + message.url.get_host() + "\r\n\r\n");
-            const std::string response = tcp.receive(4096); // Setting the buffer size to 4KB, this should be overkill for an OPTIONS request, but it's a good size for a buffer.
+            const std::string msg = "HEAD " + message.url.get_path() + " HTTP/1.1\r\n"
+                                    "Host:" + message.url.get_host() + "\r\n\r\n";
+            std::string response;
+            if (message.url.get_port() == 443)
+            {
+                tcp.write_ssl(msg);
+                response = tcp.read_ssl(4096);
+            } else
+            {
+                tcp.send(msg);
+                response = tcp.receive(4096); // Setting the buffer size to 4KB, this should be overkill for an OPTIONS request, but it's a good size for a buffer.
+            }
             parse_headers(response);
             return true;
         } catch (std::exception &e)
         {
-            std::cout << e.what() << std::endl;
             return false;
         }
     }
@@ -46,38 +67,29 @@ namespace cnet
         size_t pos = 0;
         while ((pos = body.find(delimiter)) != std::string::npos)
         {
-            try
+            std::string token = body.substr(0, pos);
+            body.erase(0, pos + 2);
+            if (token.empty()) continue;
+            if (token.find("HTTP") != std::string::npos)
             {
-                std::string token = body.substr(0, pos);
-                body.erase(0, pos + 2);
-                if (token.empty()) continue;
-                if (token.find("HTTP") != std::string::npos)
-                {
-                    message.status_code = std::stoi(token.substr(token.find(' ') + 1, 3));
-                } else
-                {
-                    const size_t colon_pos = token.find(':');
-                    const std::string key = token.substr(0, colon_pos);
-                    const std::string value = token.substr(colon_pos + 1);
-                    message.headers[key] = value;
+                message.status_code = std::stoi(token.substr(token.find(' ') + 1, 3));
+            } else
+            {
+                const size_t colon_pos = token.find(':');
+                const std::string key = token.substr(0, colon_pos);
+                const std::string value = token.substr(colon_pos + 1);
+                message.headers[key] = value;
 
-                    if (key == "Content-Length")
-                    {
-                        message.content_length = std::stoi(value);
-                    }
-                    if (key == "Content-Type")
-                    {
-                        message.content_type = value;
-                    }
-
-                    printf("%s: %s\n", key.c_str(), value.c_str());
+                if (key == "Content-Length")
+                {
+                    message.content_length = std::stoi(value);
                 }
-            } catch (std::exception &e)
-            {
-                std::cout << e.what() << std::endl;
+                if (key == "Content-Type")
+                {
+                    message.content_type = value;
+                }
             }
         }
-        std::cout << body.c_str() << std::endl;
         message.body = body;
     }
 
