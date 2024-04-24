d894d2d064a35616796eada29f4480f22aaa918e
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-04-23 21:35:23 -0400

    Add more download options and http client in main
    The main.cpp file has been updated to include additional functionality. Several new options for managing the download process have been added, including preallocation, retry count, maximum retries and maximum threads among others. Furthermore, an http_client has been implemented and integrated into the main file for making requests.

--- cnet-cli/main.cpp
+++ cnet-cli/main.cpp
@@ -4,6 +4,8 @@
 
 
 #include <cstdio>
+
+#include "http_client.h"
 #include "ANSIConsoleColors/ANSIConsoleColors.h"
 #include "cclip/cclip.hpp"
 #include "uri.h"
@@ -23,10 +25,14 @@ int main(const int argc, char *argv[])
     options_manager.add_option("m", "method", "Sets the method of the request, (GET, POST, HEAD, etc)", false, true);
     options_manager.add_option("b", "body", "Sets the body of the request", false, true);
     options_manager.add_option("hd", "header", R"(Sets the header of the request in a json format. Ex {Accept: "application/json", Content-Type: "application/json"})", false, true);
+    options_manager.add_option("a", "preallocate", "Preallocates the file size before downloading", false, false);
+    options_manager.add_option("r", "retry", "Sets the number of times to retry the download", false, true);
+    options_manager.add_option("m", "max-retires", "Sets the maximum number of retries", false, true);
+    options_manager.add_option("mt", "max-threads", "Sets the maximum number of threads to use", false, true);
+    options_manager.add_option("im", "in-memory", "Downloads the file in memory first.", false, false);
     options_manager.add_option("t", "timeout", "Sets the timeout of the request", false, true);
     options_manager.add_option("p", "parts", "Sets the number of parts to download the file in, this can increase the speed of the download", false, true);
     options_manager.add_option("f", "force", "Forces the download to start even if the file already exists", false, false);
-
 
     options_manager.parse(argc, argv);
 
@@ -50,5 +56,33 @@ int main(const int argc, char *argv[])
             }
             ConsoleColors::ResetConsoleColor();
         }
+
+        return 0;
+    }
+
+    const char *method = options_manager.is_present("m") ? options_manager.get_option("m")->argument : nullptr;
+    const char *body = options_manager.is_present("b") ? options_manager.get_option("b")->argument : nullptr;
+    const char *header = options_manager.is_present("hd") ? options_manager.get_option("hd")->argument : nullptr;
+    if (options_manager.is_present("u"))
+    {
+        if (options_manager.is_present("i"))
+        {
+            fprintf(stderr, "%s-i and -u cannot be used together, the input file will be ignored.%s\n", ConsoleColors::GetColorCode(ColorCodes::Red).c_str(), ConsoleColors::GetColorCode(ColorCodes::Default).c_str());
+        }
+        cnet::http_client client;
+        cnet::http_message message(options_manager.get_option("u")->argument);
+        if (method != nullptr)
+        {
+            try
+            {
+                message.method = cnet::parse_http_method(method);
+            } catch (std::runtime_error &e)
+            {
+                fprintf(stderr, "%s%s%s\n", ConsoleColors::GetColorCode(ColorCodes::Red).c_str(), e.what(), ConsoleColors::GetColorCode(ColorCodes::Default).c_str());
+                return 1;
+            }
+        }
+        client.make_request(message);
+        printf(message.body.c_str());
     }
 }
